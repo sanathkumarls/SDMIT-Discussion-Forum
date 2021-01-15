57f8906354a5411aceca81c0e86e3c354002d5ec
Sanath L S <sanathlslokanathapura@gmail.com>
2021-01-15 12:56:07 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,66 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
+import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+
+import { AuthService } from './auth.service';
+import { HomeComponent } from './home/home.component';
+import { CapitalizePipe } from './capitalize.pipe';
+import { AppRoutingModule } from './/app-routing.module';
+import { MenuComponent } from './menu/menu.component';
+import { MyblogsComponent } from './myblogs/myblogs.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CreateComponent } from './create/create.component';
+
+import { AngularEditorModule } from '@kolkov/angular-editor';
+import { HttpClientModule } from '@angular/common/http';
+import { PostComponent } from './post/post.component';
+import { ViewComponent } from './view/view.component';
+import { CommentsComponent } from './comments/comments.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
+
+let config = {
+  apiKey: "AIzaSyD_dbX_FzY8fzWMtqUznRSkACi1WhTyLVA",
+    authDomain: "sdmit-discussion-form.firebaseapp.com",
+    databaseURL: "https://sdmit-discussion-form.firebaseio.com",
+    projectId: "sdmit-discussion-form",
+    storageBucket: "sdmit-discussion-form.appspot.com",
+    messagingSenderId: "905076407554",
+    appId: "1:905076407554:web:62ac8ff645b3a1a55d561b"
+};
+firebase.initializeApp(config);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    HomeComponent,
+    CapitalizePipe,
+    MenuComponent,
+    MyblogsComponent,
+    ProfileComponent,
+    CreateComponent,
+    PostComponent,
+    ViewComponent,
+    CommentsComponent,
+    EditProfileComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    AngularEditorModule,
+    HttpClientModule
+  ],
+  providers: [AuthService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
